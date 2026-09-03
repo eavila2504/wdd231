c73797371b696e8fabbd270d90546e24ad4b9c5a
eavila2504 <eavilaflores@byupathway.edu>
2026-09-02 22:14:24 -0600

    site toggles

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,16 @@
+//////////////////////////media selector//////////////////////////////
 const navButton = document.querySelector('#ham-btn');
+const navLinks = document.querySelector('#nav-bar');
+
+
+//////////////////////toggle the show class off and on ///////////////////////
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
+  navLinks.classList.toggle('show');
+
 });
+
+
+
+
