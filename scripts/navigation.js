f7db1661bec43a056361d47588e63cc7804f6a68
eavila2504 <eavilaflores@byupathway.edu>
2026-09-04 21:02:28 -0600

    get last modified

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,14 +1,21 @@
 //////////////////////////media selector//////////////////////////////
 const navButton = document.querySelector('#ham-btn');
 const navLinks = document.querySelector('#nav-bar');
+const getDates = document.querySelector('#currentyear');
+const lastModified = document.querySelector('#lastModified');
 
+// Set the current year
+const currentYear = new Date().getFullYear();
+getDates.textContent = currentYear;
+
+// Set the last modified date
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+ 
 
 //////////////////////toggle the show class off and on ///////////////////////
-
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
   navLinks.classList.toggle('show');
-
 });
 
 
